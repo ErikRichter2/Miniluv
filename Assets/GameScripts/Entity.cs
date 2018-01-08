5db1f794e2e7476935adc2b6d996062c9f715340
erichter <erichter@pixelfederation.com>
2018-01-08 14:16:49 +0100

    Entity + EntityGenerator

---
+++ Assets/GameScripts/Entity.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {		
+		//PlayAnimation ("idle");
+	}
+
+	public void MoveToCheckpoint(GameObject Checkpoint) {
+		PlayAnimation ("walk");
+		iTween.MoveTo (gameObject, iTween.Hash("position", Checkpoint.transform.position,"time", 3.0f, "oncomplete", "MoveTweenComplete", "easetype", iTween.EaseType.linear));
+	}
+
+	public void MoveTweenComplete() {
+		PlayAnimation ("idle");
+		StartCoroutine (ProcessEntity());
+	}
+
+	IEnumerator ProcessEntity() {
+		yield return new WaitForSeconds (2);
+		PlayAnimation ("walk");
+		GameObject obj = GameObject.Find ("EntityGenerator");
+		transform.localScale = new Vector3(-Mathf.Abs (transform.localScale.x), transform.localScale.y, transform.localScale.z);
+		iTween.MoveTo (gameObject, iTween.Hash("position", obj.transform.position,"time", 3.0f, "oncomplete", "DestroyEntity", "easetype", iTween.EaseType.linear));
+	}
+
+	void DestroyEntity() {
+		GameObject.Destroy (gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {		
+	}
+
+	void PlayAnimation(string AnimationName) {
+		SpriteSheetAnimation[] animations = GetComponents<SpriteSheetAnimation> ();
+		foreach (SpriteSheetAnimation animation in animations) {
+			animation.Active = false;
+			if (animation.AnimationName == AnimationName) {
+				animation.StartAnimation ();
+			}
+		}
+	}
+}
---
+++ Assets/GameScripts/EntityGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityGenerator : MonoBehaviour {
+
+	public Entity EntityPrefab;
+
+	private GameObject Checkpoint;
+
+	// Use this for initialization
+	void Start () {
+		Checkpoint = GameObject.Find ("Checkpoint1");
+		StartCoroutine (Generate ());
+	}
+
+	IEnumerator Generate() {
+		while (true) {
+			Entity obj = Instantiate<Entity> (EntityPrefab);
+			obj.transform.parent = GameObject.Find ("EntityHolder").transform;
+			obj.transform.position = transform.Find ("Place").transform.position;
+			yield return new WaitForSeconds (Random.Range(0.5f, 4f));
+			obj.MoveToCheckpoint (Checkpoint.transform.Find ("Queue").gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
