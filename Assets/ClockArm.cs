5e6c85fb2555344c9b226266f09a37e3ca3721ac
erichter <erichter@pixelfederation.com>
2018-01-26 13:29:31 +0100

    added wall clocks + party members portraits

---
+++ Assets/ClockArm.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockArm : MonoBehaviour {
+
+	public int Speed;
+
+	void Update () {
+		transform.Rotate (Vector3.back * 0.5f * this.Speed);
+	}
+}
